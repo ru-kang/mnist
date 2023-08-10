1eb83768cac75cd1c2c2c7ce697d11a3eea5e58c
ru-kang <73754917+ru-kang@users.noreply.github.com>
2023-08-10 00:03:17 -0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,134 @@
+# 1.匯入 Keras 及相關模組
+import numpy as np  
+import pandas as pd  
+from keras.utils import np_utils  
+# 用來後續將 label 標籤轉為 one-hot-encoding  
+np.random.seed(10)  
+
+# 2.下載 mnist data
+from keras.datasets import mnist 
+
+# 3.讀取與查看 mnist data
+(x_train_image, y_train_label), (x_test_image, y_test_label) = mnist.load_data()  
+print('train data = ',len(x_train_image))
+print('test data =', len(x_test_image))
+print('x_test_image = ',x_train_image.shape)
+print('y_test_label =', y_test_label.shape)
+
+# 建立 plot_image 函數顯示數字影像 
+import matplotlib.pyplot as plt 
+def plot_image(image):
+    fig = plt.gcf()  
+    fig.set_size_inches(2,2)  
+    plt.imshow(image, cmap='binary') # cmap='binary' 參數設定以黑白灰階顯示.  
+    plt.show()
+
+# 1.建立 plot_images_labels_predict() 函數
+# 為了後續能很方便查看數字圖形, 真實的數字與預測結果
+def plot_images_labels_predict(images, labels, prediction, idx, num=10):  
+    fig = plt.gcf()  
+    fig.set_size_inches(12, 15)  
+    if num > 25: num = 25  
+    for i in range(0, num):  
+        ax=plt.subplot(5,5, 1+i)  
+        ax.imshow(images[idx], cmap='binary')  
+        title = "l=" + str(labels[idx])  
+        if len(prediction) > 0:  
+            title = "l={},p={}".format(str(labels[idx]), str(prediction[idx]))  
+        else:  
+            title = "l={}".format(str(labels[idx]))  
+        ax.set_title(title, fontsize=10)  
+        ax.set_xticks([]); ax.set_yticks([])  
+        idx+=1  
+    plt.show()  
+# plot_images_labels_predict(x_train_image, y_train_label, [], 0, 10)
+
+# 執行 plot_image 函數查看第 0 筆數字影像與 label 資料 
+# plot_image(x_train_image[0])
+# print(y_train_label[0])
+
+# 影像資料--------------------------------------
+# 代表 train image 總共有6萬張，每一張是28*28的圖片
+# label 也有6萬個
+# 所以要把二維的圖片矩陣先轉換成一維
+# 這裡的784是因為 28*28
+x_Train=x_train_image.reshape(60000,784).astype('float32')
+x_Test=x_test_image.reshape(10000,784).astype('float32')
+
+
+# 由於是圖片最大的是255，所以全部除以255
+x_Train_normalize=x_Train/255
+x_Test_normalize=x_Test/255
+
+# 標註資料--------------------------------------
+y_TrainOneHot=np_utils.to_categorical(y_train_label)
+y_TestOneHot=np_utils.to_categorical(y_test_label)
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+# 建立模型
+model = Sequential()
+
+# 建立輸入層和隱藏層
+model.add(Dense(units=256,input_dim=784,kernel_initializer='normal',activation='relu'))
+# 定義隱藏層神經元個數256
+# 輸入為28*28=784 個float 數字
+# 使用 normal distribution 常態分布的亂數，初始化 weight權重 bias 偏差
+# 定義激活函數為 relu
+
+
+# 建立輸出層
+model.add(Dense(units=10,kernel_initializer='normal',activation='softmax'))
+# 定義輸出層為10個 (數字0~9)
+# 也是使用常態分佈初始化
+# 定義激活函數是 softmax
+# 這裡建立的Dense 層，不用設定 input dim ，因為keras 會自動照上一層的256設定
+
+print(model.summary())
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+# 模型訓練之前要用 compele 對模型進行設定
+# loss 深度學習通常用 cross entropy 交叉嫡，訓練效果較好
+# optimizer 設定訓練時依優化的方法，在深度學習使用 adam 最優化方法，最快收斂提高準確度
+# metrics 設定評估模型的方式是 accuracy 準確率
+
+# 開始訓練
+
+train_history=model.fit(x=x_Train_normalize,y=y_TrainOneHot,
+            validation_split=0.2,epochs=10,batch_size=200,verbose=2)
+# x 是訓練資料
+# y 是label 資料
+# 設定參數 validation 切0.2起來驗證
+# epoch=10 是訓練週期為10
+# batch_size=200 每一批訓練200筆資料
+# verbose =2 顯示訓練過程
+
+# 所以以上的程式會執行10次
+# 每一次執行200筆資料 ，總共訓練資料原本有60000*0.8=48000
+# 48000/200=24 要跑240批次
+# epoch 每一次訓練週期紀錄結果在 train_history 裡面
+
+
+# 來把訓練過程畫出來
+
+def show_train_history(train_history,train,validation):
+
+  plt.plot(train_history.history[train])
+  plt.plot(train_history.history[validation])
+  plt.title('Train history')
+  plt.ylabel('train')
+  plt.xlabel('epoch')
+
+  # 設置圖例在左上角
+  plt.legend(['train','validation'],loc='upper left')
+  plt.show()
+
+show_train_history(train_history,'accuracy','val_accuracy')
+show_train_history(train_history,'loss','val_loss')
+
+# 測試集評估
+scores=model.evaluate(x_Test_normalize,y_TestOneHot)
+print()
+print('accuracy',scores[1])
+
